3339929e4a10c8439cc7d54859e39a5f958c194b
varunvasudev4 <varunvasudev4@gmail.com>
2022-08-26 20:11:46 +0530

    interview

---
+++ Problem4.js
@@ -0,0 +1,16 @@
+function fatcors(arrInp) {
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    arrOut = {}
+    for (i of arr) {
+        count = 0
+        for (j of arrInp) {
+            if (j % i == 0) {
+                count++
+            }
+        }
+        arrOut[i] = count
+    }
+    console.log(arrOut);
+}
+
+fatcors([1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30])
